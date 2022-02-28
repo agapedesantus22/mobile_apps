6947acdd83867765dd32b5c1299ccef815eaf5e4
agapedesantus22 <60992739+agapedesantus22@users.noreply.github.com>
2022-02-27 22:26:19 -0500

    Add files via upload

---
+++ com/example/capture/ExampleTestController.java
@@ -0,0 +1,14 @@
+package com.example.capture;
+
+public class ExampleTestController {
+
+    //camera test
+    //before after
+
+    //toggle test
+    //before after
+
+    //image store
+    //before after
+
+}
